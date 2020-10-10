0149197bc98f3e9329a7e14ca5346c904a72d6ba
Sijo sam <sijo@7thpillar.com>
2020-10-10 11:45:46 +0530

    cache enabled

--- src/api/v1/contollers/screenshot/index.js
+++ src/api/v1/contollers/screenshot/index.js
@@ -5,15 +5,21 @@ const dir = path.resolve();
 const takeScreenShot = require("../../utils/puppeter");
 const getScreenShot = async (req, res) => {
   const options = req.body;
+  const isDownloadFormatPdf = options.format === "pdf";
   const fileName = `${options.name}.${options.extension}`;
+  const cacheName = `${fileName}.${isDownloadFormatPdf ? `${options.dimensions && options.dimensions.pdfFormat}.` : ``}${options.dimensions.width}.${options.dimensions.height}`;
+  console.log('cacheName: ', cacheName);
+  const index = cache.keys().indexOf(cacheName);
+  console.log('index: ', index);
+  console.log('index: ', index);
+  if (index > -1) return sendData(fileName, res);
   const path = `./pdfs/${fileName}`;
-  console.log('path: ', path);
   await takeScreenShot({ ...options, path });
+  cache.set(cacheName, true, 10000);
   return sendData(fileName, res);
 };
 
 const sendData = (path, res) => {
-  console.log('path: sendData', path);
   fs.readFile(dir + "/pdfs/" + path, function (err, data) {
     if (err) throw err;
     res.setHeader("Content-Disposition", `attachment; filename=${path}`);
