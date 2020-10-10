207b1b746bb336857d1c01758bd2064e8b6cf825
Sijo sam <sijo@7thpillar.com>
2020-10-10 11:13:37 +0530

    Fixing deployment issue

--- playgroud.js
+++ playgroud.js
@@ -5,6 +5,6 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
   await page.pdf({path: 'hn.pdf', format: 'A4'});
-
+//playgroud
   await browser.close();
 })();
